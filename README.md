760015b1c36e492b287c57e1f9581d61117bb4ae
Mew Rojkanok <krojkano@uci.edu>
2025-05-06 11:23:53 -0700

    Reset

--- README.md
+++
@@ -1,5 +0,0 @@
-# inf117-skystart
-
-This document is here for reference purposes
-
-
--- skystart_website/assets/js/textEdit.js
+++
@@ -1,5 +0,0 @@
-// editor: any text that is editable 
-
-document.getElementById("editor").addEventListener("input", function() {
-    console.log("input event fired");
-}, false);
