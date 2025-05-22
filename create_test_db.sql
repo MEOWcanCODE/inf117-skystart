6a6629c830a468db9248ea89fe607a66ab857aba
Mew Rojkanok <krojkano@uci.edu>
2025-05-22 11:52:56 -0700

    create test db file

---
+++ create_test_db.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS geeklogin;
+CREATE DATABASE IF NOT EXISTS geeklogin;
+
+use geeklogin;
+
+CREATE TABLE IF NOT EXISTS accounts (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE USER 'skystart_admin'@'localhost' IDENTIFIED BY 'My$Password0';
+GRANT ALL ON geeklogin TO 'skystart_admin'@'localhost';
+FLUSH PRIVILEGES;
